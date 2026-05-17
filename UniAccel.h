eceea282cd9a122648818d944bb1c0bd42f37aff
ThemeHackers <tigerzaza5678@gmail.com>
2026-05-17 20:40:38 +0700

    Clean code: remove comments from UniAccel.h

--- UniAccel.h
+++ UniAccel.h
@@ -1,21 +1,16 @@
 #ifndef UNIACCEL_H
 #define UNIACCEL_H
-
 #include <Arduino.h>
 #include <WebSocketsClient.h>
 #include <ArduinoJson.h>
-
 #ifndef SESSION_KEY
 static uint8_t session_key[16] = {0x5A, 0xA5, 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66};
 #endif
-
-
 #define BUILD_PHASE_SETUP    "setup"
 #define BUILD_PHASE_PREPARE  "prepare"
 #define BUILD_PHASE_COMPILE  "compile"
 #define BUILD_PHASE_LINK     "link"
 #define BUILD_PHASE_VERIFY   "verify"
-
 struct BuildStatus {
   float progress;       
   const char* phase;     
@@ -23,7 +18,6 @@ struct BuildStatus {
   bool isBuilding;      
   unsigned long startTime;
 };
-
 extern WebSocketsClient webSocket;
 extern bool accelConnected;
 extern bool accelStopRequested;
@@ -42,7 +36,6 @@ extern float gpuPwr;
 extern uint16_t gpuClk;
 extern BuildStatus buildStatus;
 extern bool showBuildProgress;
-
 extern const char CLR_RST[] PROGMEM;
 extern const char CLR_RED[] PROGMEM;
 extern const char CLR_GRN[] PROGMEM;
@@ -51,7 +44,6 @@ extern const char CLR_BLU[] PROGMEM;
 extern const char CLR_MAG[] PROGMEM;
 extern const char CLR_CYN[] PROGMEM;
 extern const char CLR_WHT[] PROGMEM;
-
 void initUniAccel();
 void loopUniAccel();
 void handleAccelCommand(char* args);
@@ -65,10 +57,7 @@ void kprintColor_P(const char *pgmColor);
 void kprintProgmem(const char *pgmStr);
 void displayBuildProgress(float progress, const char* phase, const char* message);
 void displayTelemetryBox();
-
-
 void hashPass(const char* input, char* output, const uint8_t* salt);
 bool secureEquals(const char* a, const char* b, size_t len);
 void generateNewSalt(uint8_t* salt);
-
 #endif
