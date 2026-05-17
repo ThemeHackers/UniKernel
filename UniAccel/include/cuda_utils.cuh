ee72cb9ebc4c6594b0b234e756f78ed7b28f9240
ThemeHackers <tigerzaza5678@gmail.com>
2026-05-17 20:40:41 +0700

    Clean code: remove comments from cuda_utils.cuh

--- UniAccel/include/cuda_utils.cuh
+++ UniAccel/include/cuda_utils.cuh
@@ -2,31 +2,50 @@
 #define CUDA_UTILS_CUH
 
 #include <cuda_runtime.h>
+#include <device_launch_parameters.h>
 #include <stdio.h>
 
-#define CUDA_CHECK(call) \
-    do { \
-        cudaError_t err = call; \
-        if (err != cudaSuccess) { \
-            fprintf(stderr, "CUDA Error at %s:%d - %s\n", __FILE__, __LINE__, cudaGetErrorString(err)); \
-        } \
+#define CUDA_CHECK(call)                                                              \
+    do {                                                                              \
+        cudaError_t err = call;                                                       \
+        if (err != cudaSuccess) {                                                     \
+            fprintf(stderr, "CUDA Error at %s:%d - %s (%s)\n",                        \
+                    __FILE__, __LINE__, cudaGetErrorString(err), cudaGetErrorName(err)); \
+        }                                                                             \
     } while (0)
+
+#define WARP_SIZE 32
+#define FULL_MASK 0xFFFFFFFF
 
 __device__ __forceinline__ float3 add_float3(float3 a, float3 b) {
     return make_float3(a.x + b.x, a.y + b.y, a.z + b.z);
+}
+
+__device__ __forceinline__ float3 sub_float3(float3 a, float3 b) {
+    return make_float3(a.x - b.x, a.y - b.y, a.z - b.z);
 }
 
 __device__ __forceinline__ float3 mul_float3(float3 a, float s) {
     return make_float3(a.x * s, a.y * s, a.z * s);
 }
 
+__device__ __forceinline__ float length_sq(float3 v) {
+    return v.x*v.x + v.y*v.y + v.z*v.z;
+}
+
 __device__ __forceinline__ float length(float3 v) {
-    return sqrtf(v.x*v.x + v.y*v.y + v.z*v.z);
+    return sqrtf(length_sq(v));
 }
 
 __device__ __forceinline__ float3 normalize(float3 v) {
-    float len = length(v);
-    return (len > 0.0f) ? make_float3(v.x/len, v.y/len, v.z/len) : make_float3(0.0f, 0.0f, 0.0f);
+    float invLen = rsqrtf(length_sq(v) + 1e-10f);
+    return mul_float3(v, invLen);
+}
+
+__device__ __forceinline__ float warpReduceSum(float val) {
+    for (int offset = WARP_SIZE / 2; offset > 0; offset /= 2)
+        val += __shfl_down_sync(FULL_MASK, val, offset);
+    return val;
 }
 
 extern __constant__ float3 c_axis;
